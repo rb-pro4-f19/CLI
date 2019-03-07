900632fbf508bd7514ee93d7452dfbce454026d4
Martin Androvich <martinandrovich@gmail.com>
2019-03-08 00:43:50 +0100

    Update CLI

--- src/main.cpp
+++ src/main.cpp
@@ -10,29 +10,21 @@
 // All essential functionality is located in system.cpp, where main.cpp simply acts
 // as an entry point and overview of the system.
 
-// commands map
-std::unordered_map<std::string, void(*)(std::string args)> main_commands = 
+std::vector<cmd> commands =
 {
 	{ "connect",		[](std::string args) { return; } },
 	{ "read",			[](std::string args) { return; } },
 	{ "write",			[](std::string args) { return; } },
 	{ "log",			[](std::string args) { return; } },
-	{ "mode",			[](std::string args) { return; } }
-};
-
-std::unordered_map<std::string, void(*)(std::string args)> mode_commands =
-{
-	{ "demo",			[](std::string args) { return; } },
-	{ "free",			[](std::string args) { return; } },
-	{ "auto",			[](std::string args) { return; } }
-};
-
-std::unordered_map<std::string, void(*)(std::string args)> func_commands =
-{ // define functions per mode
-	{ "init",			[](std::string args) { return; } },
-	{ "cal",			[](std::string args) { return; } },
-	{ "set_pos",		[](std::string args) { return; } },
-	{ "get_pos",		[](std::string args) { return; } }
+	{ "mode",			[](std::string args) { return; }, {
+		{ "standby",		[](std::string args) { return; } },
+		{ "manual",			[](std::string args) { return; } },
+		{ "auto",			[](std::string args) { return; } }
+	}},
+	{ "set",			[](std::string args) { return; }, {
+		{ "pos",			[](std::string args) { return; } },
+		{ "specs",			[](std::string args) { return; } },
+	}}
 };
 
 // main function
--- src/system.h
+++ src/system.h
@@ -8,6 +8,14 @@ extern std::unordered_map<std::string, void(*)(std::string args)> command_map;
 
 // Modes
 
+struct cmd
+{
+	std::string name;
+	void(*func)(std::string args) = nullptr;
+	std::vector<cmd> args = {};
+	std::string help = "No information available.";
+};
+
 enum class SYSTEM_MODE 
 {
 	STANDBY,
