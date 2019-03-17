f492ac131b4fba161cbe179867adfb512c02692f
Martin Androvich <martinandrovich@gmail.com>
2019-03-17 19:40:24 +0100

    Create CLI module
    Used for logging, parsing input, showing msgbox etc.

---
+++ src/cli.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <sstream>
+#include <thread>
+#include <algorithm>
+
+#include <windows.h>
+
+#define newcmd new cli::cmd_container(
+
+namespace cli
+{
+
+	struct cmd;
+	struct cmd_container;
+
+	struct cmd
+	{
+		std::string name;
+		void(*func)(std::string args) = nullptr;
+		cmd_container* child_cmds = nullptr;
+		std::string help = "No information available.";
+
+		bool has_children() { return this->child_cmds != nullptr; }
+	};
+
+	struct cmd_container
+	{
+		cmd_container() {};
+
+		cmd_container(std::initializer_list<cmd> init)
+			: commands(init) {};
+
+		std::vector<cmd> commands = {};
+
+		int size()
+		{
+			return commands.size();
+		}
+
+		bool has_command(std::string cmd_name)
+		{
+			return (std::any_of(this->commands.begin(), this->commands.end(), [&](cmd &cmd_element) { return cmd_element.name == cmd_name; }));
+		}
+
+		cmd* get_command(std::string cmd_name)
+		{
+			auto it = std::find_if(this->commands.begin(), this->commands.end(), [&](cmd &cmd_element) { return cmd_element.name == cmd_name; });
+			return &(*it);
+		} // operator overload -> cmd_container("cmd");
+
+		cmd* operator [] (std::string cmd_name)
+		{
+			return this->get_command(cmd_name);
+		}
+	};
+
+	void init(cli::cmd_container commands);
+	void get_input();
+
+	void msgbox(std::string msg, std::string title);
+
+	void log_insert(std::string);
+	void log_reset(std::string);
+}
